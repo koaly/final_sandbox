3af581fdc47c782b4923132ee8de09800b0a6377
koaly_ <koaly@koaly.me>
2020-02-18 01:47:08 +0700

    update blkio and network

--- create_sandbox.sh
+++ create_sandbox.sh
@@ -4,6 +4,17 @@ user="app"
 
 while :; do
 	case $1 in
+		-c|--config)
+			if [ "$2" ]; then
+                config="$2"
+                shift
+            else
+                die 'ERROR: "--config" requires a non-empty option argument.'
+            fi
+		;;
+		--config=?*)
+			config="${1#*=}"
+		;;
 		-U|--update)
 			update="update"
 		;;
@@ -33,17 +44,25 @@ done
 sandbox_name="$1"
 user_dir="$sandbox_name/home/$user"
 
+if [ "$config" ]; then
+	. $config
+fi
+
 # echo $update
 # echo $dir
 # echo $sandbox_name
 
 if [ "$update" ]; then
-	sudo cp -avr /{bin,dev,etc,lib,lib64,run,sbin,usr} $dir
+	sudo cp -r --no-dereference --preserve=mode,ownership,timestamps --verbose /{bin,dev,etc,lib,lib64,run,sbin,usr} $dir
+	sudo rm -rf $dir/etc/resolv.conf
+	echo nameserver 8.8.8.8 | sudo tee $dir/etc/resolv.conf
 else
 	mkdir $sandbox_name
 	mkdir $sandbox_name/home
 	mkdir $user_dir
-	cp -avr $dir $user_dir
+	sudo cp -r --no-dereference --preserve=mode,ownership,timestamps --verbose $dir $user_dir
 	sudo chown -R $user $user_dir
-	sudo cp -avrn /{bin,dev,etc,lib,lib64,run,sbin,usr} $sandbox_name
+	sudo cp -r --no-dereference --preserve=mode,ownership,timestamps --verbose /{bin,dev,etc,lib,lib64,run,sbin,usr} $sandbox_name
+	sudo rm -rf $sandbox_name/etc/resolv.conf
+	echo nameserver 8.8.8.8 | sudo tee $sandbox_name/etc/resolv.conf
 fi
--- run_sandbox.sh
+++ run_sandbox.sh
@@ -1,17 +1,30 @@
 #!/bin/bash
 
-user_t="--userspec=app"
+user="app"
 
-cpu_period_t="-r cpu.cfs_period_us=100000"
-cpu_quota_t="-r cpu.cfs_quota_us=10000"
-memory_t="-r memory.limit_in_bytes=1G"
+io_read="1048576"
+io_write="1048576"
+cpu_period="100000"
+cpu_quota="10000"
+memory="1G"
 
 cgroup_dir=$$
 
 while :; do
 	case $1 in
 		--disable_network)
-			network='--net'
+			disable_network="true"
+		;;
+		-n|--network)
+			if [ "$2" ]; then
+                network="$2"
+                shift
+            else
+                die 'ERROR: "--network" requires a non-empty option argument.'
+            fi
+		;;
+		--network=?*)
+			network="${1#*=}"
 		;;
 		-c|--config)
 			if [ "$2" ]; then
@@ -57,7 +70,7 @@ while :; do
 		--cpu_quota=?*)
             cpu_quota="${1#*=}"
         ;;
-		--memory)
+		-m|--memory)
             if [ "$2" ]; then
                 memory="$2"
                 shift
@@ -67,6 +80,31 @@ while :; do
         ;;
 		--memory=?*)
             memory="${1#*=}"
+        ;;
+		--unlimit_io)
+			unlimit_io='true'
+		;;
+		--io_read)
+            if [ "$2" ]; then
+                io_read="$2"
+                shift
+            else
+                die 'ERROR: "--io_read" requires a non-empty option argument.'
+            fi
+        ;;
+		--io_read=?*)
+            io_read="${1#*=}"
+        ;;
+		--io_write)
+            if [ "$2" ]; then
+                io_write="$2"
+                shift
+            else
+                die 'ERROR: "--io_write" requires a non-empty option argument.'
+            fi
+        ;;
+		--io_write=?*)
+            io_write="${1#*=}"
         ;;
 		--)
 	    	shift
@@ -82,10 +120,6 @@ done
 
 if [ "$config" ]; then
 	. $config
-fi
-
-if [ "$disable_network" ]; then
-	network='--net'
 fi
 
 if [ "$user" ]; then
@@ -109,8 +143,42 @@ for arg in $@; do
 	option+="${arg} "
 done
 
+sandbox_ip=$((($$-1)/256)).$((($$-1)%256))
+sudo ip netns add sandbox$$
+sudo ip netns exec sandbox$$ ip addr add 127.0.0.1/8 dev lo
+sudo ip netns exec sandbox$$ ip link set lo up
+sudo ip link add veth$$ type veth peer name veth$$-in
+sudo ip link set veth$$ up
+sudo ip link set veth$$-in netns sandbox$$ up
+sudo ip addr add 100.$sandbox_ip.1/30 dev veth$$
+sudo ip netns exec sandbox$$ ip addr add 100.$sandbox_ip.2/30 dev veth$$-in
+sudo ip netns exec sandbox$$ ip route add default via 100.$sandbox_ip.1 dev veth$$-in
+
+echo 1 | sudo tee /proc/sys/net/ipv4/ip_forward
+ext_if=$(ip route get 8.8.8.8 | grep 'dev' | awk '{ print $5 }')
+sudo iptables -I POSTROUTING -t nat -s 100.$sandbox_ip.2/32 -o ${ext_if} -j MASQUERADE
+sudo iptables -I FORWARD -i veth$$ -o ${ext_if} -j ACCEPT
+sudo iptables -I FORWARD -i ${ext_if} -o veth$$ -j ACCEPT
+sudo mkdir -p /etc/netns/sandbox$$
+
 echo "****Please remember PID for sandbox monitoring.****"
 echo "Sandbox's PID is "$BASHPID
+echo nameserver 8.8.8.8 | sudo tee /etc/netns/sandbox$$/resolv.conf
+
+if [ "$network" ]; then
+	if [ $(head -1 $network) -eq 1 ]; then
+	    sudo ip netns exec sandbox$$ iptables -A OUTPUT -d 8.8.8.8 -j ACCEPT; \
+    	for addr in `tail -n +2 $network`; do \
+        	sudo ip netns exec sandbox$$ iptables -A OUTPUT -d $addr -j ACCEPT; \
+    	done
+    	sudo ip netns exec sandbox$$ iptables -A OUTPUT -j DROP
+	else
+    	for addr in `tail -n +2 $network`; do \
+        	sudo ip netns exec sandbox$$ iptables -A OUTPUT -d $addr -j DROP; \
+    	done
+	fi
+fi
+
 echo "[Press any key to continue]"
 read
 
@@ -119,9 +187,33 @@ read
 # printf "Argument user is %s\n" "$user"
 # printf "Argument option is %s\n" "$option"
 
-sudo unshare $network --mount --fork bash -c "
-	cgcreate -g cpu,memory,blkio,devices,freezer:/sandbox/$cgroup_dir
-	cgset $cpu_period_t $cpu_quota_t $memory_t sandbox/$cgroup_dir
-	cgexec -g cpu,memory,blkio,devices,freezer:/sandbox/$cgroup_dir \
-	chroot $user_t $dir $option
-"
+sudo cgcreate -g cpu,memory,blkio:/sandbox/$cgroup_dir
+sudo cgset $cpu_period_t $cpu_quota_t $memory_t sandbox/$cgroup_dir
+
+if [ "$disable_network" ]; then
+	if [ "$unlimit_io" ]; then
+		sudo unshare --net --mount --fork bash -c "
+			cgexec -g cpu,memory:/sandbox/$cgroup_dir \
+			chroot $user_t $dir $option
+		"
+	else
+		sudo cat /proc/partitions | sed "s/ \+/ /g" | cut -d" " -f2,3 --output-delimiter=":" | grep "[0-9]" | while read dev; do sudo cgset -r blkio.throttle.read_bps_device="$dev $io_read" -r blkio.throttle.write_bps_device="$dev $io_write" sandbox/$cgroup_dir ; done
+		sudo unshare --net --mount --fork bash -c "
+			cgexec -g cpu,memory,blkio:/sandbox/$cgroup_dir \
+			chroot $user_t $dir $option
+		"
+	fi
+else
+	if [ "$unlimit_io" ]; then
+		sudo cgexec -g cpu,memory:/sandbox/$cgroup_dir \
+			sudo ip netns exec sandbox$$ unshare --mount --fork bash -c "
+			chroot $user_t $dir $option
+		"
+	else
+		sudo cat /proc/partitions | sed "s/ \+/ /g" | cut -d" " -f2,3 --output-delimiter=":" | grep "[0-9]" | while read dev; do sudo cgset -r blkio.throttle.read_bps_device="$dev $io_read" -r blkio.throttle.write_bps_device="$dev $io_write" sandbox/$cgroup_dir ; done
+		sudo cgexec -g cpu,memory,blkio:/sandbox/$cgroup_dir \
+			sudo ip netns exec sandbox$$ unshare --mount --fork bash -c "
+			chroot $user_t $dir $option
+		"
+	fi
+fi
